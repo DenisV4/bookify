ea4c6e228844d99f11a3a7f0bdbfc7a5e55b9a59
Denis <denisvyrov@yandex.ru>
2024-09-10 12:58:04 +0500

    redis configuration changed

--- server/src/main/java/example/bookify/server/config/RedisConfig.java
+++ server/src/main/java/example/bookify/server/config/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
@@ -36,7 +37,7 @@ public class RedisConfig {
         configuration.setHostName(redisProperties.getHost());
         configuration.setPort(redisProperties.getPort());
 
-        return new JedisConnectionFactory(configuration);
+        return new LettuceConnectionFactory(configuration);
     }
 
     public class RefreshTokenKeyspaceConfiguration extends KeyspaceConfiguration {
